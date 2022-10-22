b70107470c8af8aa152fb8864fcc909bdcb6a576
Martin Wiulsrød <wiu753@gmail.com>
2022-10-22 13:50:56 -0400

    Add solution to traveling salesman using random method

--- obligatory1/index.js
+++ obligatory1/index.js
@@ -1,27 +1,49 @@
-let cities = [];
+let citiesLeftToVisit = [];
+let numberOfCities = 50;
+let citiesVisited = [];
+let costOfTravel = 0;
 
-// There are 50 cities
-for (let i = 0; i < 50; i++) {
+// Pick a random number (min and max is inclusive)
+let randomIntFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// Create array of cities
+for (let i = 0; i < numberOfCities; i++) {
   // Each city has 49 connections, one to each other city
-  cities.push([]);
-  for (let j = 0; j < 50; j++) {
-    cities[i].push(-1);
+  let cityObj = {
+    id: i,
+    connections: [],
+  };
+  citiesLeftToVisit.push(cityObj); // Add the new city object to the array
+  for (let j = 0; j < numberOfCities; j++) { // Fill the entire array with the number -1
+    citiesLeftToVisit[i].connections.push(-1);
   }
 }
 
-// Generate the graph (cities)
-for (let i = 0; i < 50; i++) {
-  for (let j = 0; j < 50; j++) {
-    // Check that we are not updating a link to the same city and that we only
-    // update cities once (not override)
-    if (cities[i][j] == -1 && i !== j) {
+// Generate the graph containing the connections between cities
+for (let i = 0; i < numberOfCities; i++) {
+  for (let j = 0; j < numberOfCities; j++) {
+    // Check that we are not updating a link to the same city and that we only update cities once (not override)
+    if (citiesLeftToVisit[i].connections[j] == -1 && i !== j) {
       let random = Math.floor(Math.random() * 9 + 1);
-      cities[i][j] = random
-      cities[j][i] = random
+      citiesLeftToVisit[i].connections[j] = random;
+      citiesLeftToVisit[j].connections[i] = random;
     }
   }
 }
 
+// Pick a random city to start in
+let currentCityIndex = randomIntFromInterval(0, citiesLeftToVisit.length - 1); // Add -1 because length is 1 more
+let currentCity = citiesLeftToVisit[currentCityIndex];
+citiesVisited.push(citiesLeftToVisit.splice(currentCityIndex, 1));
 
+while (citiesLeftToVisit.length > 0) {
+  nextCityIndex = randomIntFromInterval(0, citiesLeftToVisit.length - 1); // Pick a random city
+  nextCity = citiesLeftToVisit[nextCityIndex];
+  costOfTravel += nextCity.connections[currentCity.id]; // Add the cost of the connection
+  currentCity = nextCity;
+  citiesVisited.push(citiesLeftToVisit.splice(nextCityIndex, 1)); // Remove the city from the array and add to visited
+}
 
-console.log(cities);
+console.log("Cost of travel", costOfTravel);
