c9976c66dc1cfbac1e8dc7a4d31d202b4aed4698
Martin Wiulsrød <wiu753@gmail.com>
2022-10-25 15:50:55 -0400

    Refactor random and randomImproved for bette readability

--- obligatory1/random.js
+++ obligatory1/random.js
@@ -1,53 +1,54 @@
-let citiesLeftToVisit = [];
-let numberOfCities = 4;
-let citiesVisited = [];
-let costOfTravel = 0;
+let cities = [],
+    tour = [],
+    trail = []
+let amount = 500,
+    cost = 0
 
-// Pick a random number (min and max is inclusive)
+// Pick a random number, min and max is inclusive
 let randomIntFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-// Create array of cities
-for (let i = 0; i < numberOfCities; i++) {
-  // Each city has 49 connections, one to each other city
-  let cityObj = {
-    id: i,
-    connections: [],
-  };
-  citiesLeftToVisit.push(cityObj); // Add the new city object to the array
-  for (let j = 0; j < numberOfCities; j++) { // Fill the entire array with the number -1
-    citiesLeftToVisit[i].connections.push(-1);
-  }
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-// Generate the graph containing the connections between cities
-for (let i = 0; i < numberOfCities; i++) {
-  for (let j = 0; j < numberOfCities; j++) {
-    // Check that we are not updating a link to the same city and that we only update cities once (not override)
-    if (citiesLeftToVisit[i].connections[j] == -1 && i !== j) {
-      let random = Math.floor(Math.random() * 9 + 1);
-      citiesLeftToVisit[i].connections[j] = random;
-      citiesLeftToVisit[j].connections[i] = random;
+// Push cities into array
+for (let i = 0; i < amount; i++) {
+    let cityObj = {
+        id: i,
+        connections: [],
     }
-  }
+
+    cities.push(cityObj)
+
+    for (let j = 0; j < amount; j++) {
+        cities[i].connections.push(-1)
+    }
+}
+
+// Loop over all the cities and their connections to set a random cost
+for (let i = 0; i < amount; i++) {
+    for (let j = 0; j < amount; j++) {
+        // Update all connections that are -1 and not a connection to itself
+        if (cities[i].connections[j] == -1 && i !== j) {
+            let randomCost = Math.floor(Math.random() * 9 + 1)
+            cities[i].connections[j] = cities[j].connections[i] = randomCost
+        }
+    }
 }
 
 // Pick a random city to start in
-let currentCityIndex = randomIntFromInterval(0, citiesLeftToVisit.length - 1); // Add -1 because length is 1 more
-let currentCity = citiesLeftToVisit[currentCityIndex];
-citiesVisited.push(citiesLeftToVisit.splice(currentCityIndex, 1)[0]);
+let currentIndex = randomIntFromInterval(0, cities.length - 1)
+let currentCity = cities[currentIndex]
+tour.push(cities.splice(currentIndex, 1)[0])
 
-while (citiesLeftToVisit.length > 0) {
-  nextCityIndex = randomIntFromInterval(0, citiesLeftToVisit.length - 1); // Pick a random city
-  nextCity = citiesLeftToVisit[nextCityIndex];
-  costOfTravel += nextCity.connections[currentCity.id]; // Add the cost of the connection
-  currentCity = nextCity;
-  citiesVisited.push(citiesLeftToVisit.splice(nextCityIndex, 1)[0]); // Remove the city from the array and add to visited
+// Loop over all cities, adding to the total cost and tour
+while (cities.length > 0) {
+    nextIndex = randomIntFromInterval(0, cities.length - 1)
+    nextCity = cities[nextIndex]
+    cost += nextCity.connections[currentCity.id]
+    trail.push(nextCity.connections[currentCity.id])
+    currentCity = nextCity
+    tour.push(cities.splice(nextIndex, 1)[0])
 }
 
-console.log("Cost of travel", costOfTravel);
-// console.log(citiesVisited)
-// console.log(citiesVisited.length)
-
-console.log(citiesVisited)
+console.log("The tour: ", tour)
+console.log("The trail: ", trail)
+console.log("Cost of travel: ", cost)
---
+++ obligatory1/randomImproved.js
@@ -0,0 +1,90 @@
+let cities = [],
+    tour = [],
+    trail = []
+let amount = 10,
+    cost = 0,
+    greedyIterations = 5000,
+    greedyCount = 0
+
+// Pick a random number (min and max is inclusive)
+let randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+// Calculate the cost of a tour
+let calculateCost = (tour) => {
+    let prev,
+        current,
+        cost = 0
+
+    for (let i = 1; i < tour.length; i++) {
+        prev = tour[i - 1].id
+        current = tour[i]
+        cost += current.connections[prev]
+    }
+
+    return cost
+}
+
+// Push cities into array
+for (let i = 0; i < amount; i++) {
+    let cityObj = {
+        id: i,
+        connections: [],
+    }
+
+    cities.push(cityObj)
+
+    for (let j = 0; j < amount; j++) {
+        cities[i].connections.push(-1)
+    }
+}
+
+// Loop over all the cities and their connections to set a random cost
+for (let i = 0; i < amount; i++) {
+    for (let j = 0; j < amount; j++) {
+        // Check that we are not updating a link to the same city and that we only update cities once (not override)
+        if (cities[i].connections[j] == -1 && i !== j) {
+            let random = Math.floor(Math.random() * 9 + 1)
+            cities[i].connections[j] = cities[j].connections[i] = random
+        }
+    }
+}
+
+// Pick a random city to start in
+let currentCityIndex = randomIntFromInterval(0, cities.length - 1) // Add -1 because length is 1 more
+let currentCity = cities[currentCityIndex]
+tour.push(cities.splice(currentCityIndex, 1)[0])
+
+// Loop over all cities, adding to the total cost and tour
+while (cities.length > 0) {
+    nextCityIndex = randomIntFromInterval(0, cities.length - 1)
+    nextCity = cities[nextCityIndex]
+    cost += nextCity.connections[currentCity.id]
+    trail.push(nextCity.connections[currentCity.id])
+    currentCity = nextCity
+    tour.push(cities.splice(nextCityIndex, 1)[0])
+}
+
+console.log("The cost trail: ", trail)
+console.log("Initial cost of tour: ", cost)
+
+// Greedy improvement algorithm
+while (greedyCount < greedyIterations) {
+    let swap,
+        firstRandomCity = randomIntFromInterval(0, tour.length - 1),
+        secondRandomCity = randomIntFromInterval(0, tour.length - 1),
+        temp = [...tour]
+
+    swap = temp[firstRandomCity]
+    temp[firstRandomCity] = temp[secondRandomCity]
+    temp[secondRandomCity] = swap
+
+    if (calculateCost(temp) < cost) {
+        cost = calculateCost(tour)
+        tour = temp
+    }
+    greedyCount++
+}
+
+console.log("Cost of tour after greedy improvement heuristic", cost)
